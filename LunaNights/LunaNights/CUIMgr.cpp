b47da954f8e0442777060d1a769fe7a0c276b2d6
SDTR <sndvsdtr@gmail.com>
2025-02-14 14:38:52 +0900

    이전과 동일

---
+++ LunaNights/LunaNights/CUIMgr.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "CUIMgr.h"
+
+CUIMgr::CUIMgr()
+{
+}
+
+CUIMgr::~CUIMgr()
+{
+}
+
+void CUIMgr::Initialize()
+{
+}
+
+void CUIMgr::Update()
+{
+}
+
+void CUIMgr::Late_Update()
+{
+}
+
+void CUIMgr::Render(HDC hDC)
+{
+}
+
+void CUIMgr::Release()
+{
+}
