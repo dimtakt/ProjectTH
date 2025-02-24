a965f029e84767b14bd380d434f63ac1c7832a78
SDTR <sndvsdtr@gmail.com>
2025-02-24 11:59:42 +0900

    .

---
+++ LunaNights/LunaNights/CBoss_HonMeirin.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "CBoss_HonMeirin.h"
+
+CBoss_HonMeirin::CBoss_HonMeirin()
+{
+}
+
+CBoss_HonMeirin::~CBoss_HonMeirin()
+{
+}
+
+void CBoss_HonMeirin::Initialize()
+{
+}
+
+int CBoss_HonMeirin::Update()
+{
+	return 0;
+}
+
+void CBoss_HonMeirin::Late_Update()
+{
+}
+
+void CBoss_HonMeirin::Render(HDC hDC)
+{
+}
+
+void CBoss_HonMeirin::Release()
+{
+}
